69258347fc555bfde1d2b3623893b11ca2680a0f
Joel Parker Henderson <joel@joelparkerhenderson.com>
2023-04-28 15:16:50 +0100

    Publish

---
+++ README.md
@@ -0,0 +1,21 @@
+# Demo Rust Ed25519 Dalek
+
+Demonstration of:
+
+* Rust programming language
+
+* Ed25519 elliptic curve cryptography message signing
+
+* ed25519-dalek crate that implements message signing
+
+This demo uses ed25519-dalek version 2. The v2 API is signicantly different than v1.
+
+
+## Tracking
+
+* Package: demo-rust-ed25519-dalek
+* Version: 1.0.0
+* Created: 2023-04-28T14:16:04Z
+* Updated: 2023-04-28T14:16:04Z
+* License: MIT or Apache-2.0 or GPL-2.0 or GPL-3.0 or contact us for more
+* Contact: Joel Parker Henderson (joel@sixarm.com)
---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use rand::rngs::OsRng;
+
+use ed25519_dalek::{Signer, SigningKey, Signature, Verifier, VerifyingKey};
+
+pub fn main() {
+    // Create a message that we will sign.
+    let message: &[u8] = b"Hello, World!";
+
+    // Load a random number generator (RNG).
+    let mut rng = OsRng{};
+    
+    // Generate a signing key a.k.a. a keypair
+    let signing_key: SigningKey = SigningKey::generate(&mut rng);
+
+    // Sign the message
+    let signature: Signature = signing_key.sign(message);
+    
+    // Get the verifying key a.k.a. the keypair private key.
+    let verifying_key: VerifyingKey = signing_key.verifying_key();
+
+    // Verify the signature.
+    assert!(verifying_key.verify(message, &signature).is_ok());
+}
